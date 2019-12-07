ffe6729cd6378d8d1c963c846051e635808ea5bc
diepi <55230119+diepi@users.noreply.github.com>
2019-12-07 11:34:06 +0100

    Update connect4_ngocdiepdo.py

--- connect4_ngocdiepdo.py
+++ connect4_ngocdiepdo.py
@@ -3,15 +3,6 @@
 """
 Created on Fri Apr 28 12:09:58 2017
 
-This Python module runs connect4 game. 
-It allows the game to be played by 2 humans, a human against a computer, or computer against itself.
-In this game two players take turns dropping objects from the top into a seven-column, six-row vertically suspended grid. 
-The pieces fall straight down, occupying the next available space within the column. 
-The objective of the game is to be the first to form a horizonala, vertical, or diagonal line of four of one's own discs.
-The module should also allow the current game to be saved, and a previously saved game to be loaded and continued.
-
-Ngoc Diep Do
-SI:9643942
 """
 
 from copy import deepcopy 
